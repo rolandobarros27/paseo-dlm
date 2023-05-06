28520b0f9887bb95846b156f3420720684a8b2e2
rbarros <rolandobarros27@gmail.com>
2023-05-05 21:04:56 -0400

    Firts commit of the project

---
+++ src/main/java/com/paseodlm/paseodlm/PaseodlmApplication.java
@@ -0,0 +1,13 @@
+package com.paseodlm.paseodlm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaseodlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaseodlmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paseodlm/paseodlm/PaseodlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paseodlm.paseodlm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaseodlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
